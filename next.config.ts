7f7fd04e4e8d128bc0efb83e33245035e0e2f76f
autmationsolution-a11y <autmationsolution@gmail.com>
2026-01-02 09:44:27 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/dashboard",
-  assetPrefix: "/dashboard/",
   images: { unoptimized: true },
 };
 
